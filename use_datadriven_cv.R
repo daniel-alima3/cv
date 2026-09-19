a611d6bcf7d063415b1991b04b29ab648ddd9cfb
Daniel <dalima.dev@gmail.com>
2026-09-19 13:49:56 -0300

    Fix data_location URL by removing duplicate protocol prefix

--- use_datadriven_cv.R
+++ use_datadriven_cv.R
@@ -1,8 +1,7 @@
 library(datadrivencv)
-library(here)
 use_datadriven_cv(
   full_name = "Daniel Alves",
-  data_location = "https://https://docs.google.com/spreadsheets/d/1qv0BxGQxGOFAHLhKoA4qQ4qps_uW_wzmhFEkFW9Y5tk/edit?gid=340636497#gid=340636497",
+  data_location = "https://docs.google.com/spreadsheets/d/1qv0BxGQxGOFAHLhKoA4qQ4qps_uW_wzmhFEkFW9Y5tk/edit?gid=340636497#gid=340636497",
   pdf_location = "https://github.com/daniel-alima3/cv/raw/master/dalima_cv.pdf",
   html_location = "daniel-alima3.github.io/cv/",
   source_location = "https://github.com/daniel-alima3/cv",
